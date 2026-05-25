96ecc143725ad82a066bed3df083098726ee51b0
alazharalyaarubi <alazharsulaiman07@gmail.com>
2026-05-25 15:25:33 +0400

    Updated Course.h

--- Course/Course.h
+++ Course/Course.h
@@ -1,24 +1,44 @@
 #ifndef COURSE_H
 #define COURSE_H
 
+#include <iostream>
 #include <string>
 #include "Instructor.h"
-#include "Lesson.h"
 using namespace std;
 
 class Course {
 private:
     string code;
     string name;
+    string day;
+    int startTime;
+    int endTime; 
+    int capacity;
+    int registered;
     Instructor* instructor;  // Pointer used to assign instructor
-    Lesson lesson;           // Composition: course has a lesson
+
 public:
     Course();
-    Course(string c, string n, Lesson l);
+    Course(string c, string n, string d, int s, int e, int cap);
+    ~Course();
+    
     string getCode() const;
-    string getName() const;
+    string getDay() const;
+    int getStartTime() const;
+    int getEndTime() const;
+
     void assignInstructor(Instructor* i);
+
+    bool isFull() const;
+    bool conflict(Course c) const;
+
+    void addStudent();
+    void removeStudent();
+
+    bool operator==(Course c);
+
     void showCourse() const;
+    
 };
 
 #endif
