86ae77c8d30b684b106f746488558f98e93ac012
AhmedAlfarsiGU <ahmedalfarsigu@gmail.com>
2026-05-25 15:39:11 +0400

    Update User.cpp

--- User/User.cpp
+++ User/User.cpp
@@ -1,23 +1,33 @@
 #include "User.h"
 
 User::User() {
-    id = "0";
     name = "Unknown";
+    address = "Unknown";
+    email = "Unknown";
+    phone = "Unknown";
 }
 
-User::User(string i, string n) {
-    id= i;
-    name =n;
+User::User(string n, string a, string e, string p) {
+    name = n;
+    address = a;
+    email = e;
+    phone = p;
+}
+
+User::~User() {
 }
 
 void User::showRole() const {
-        cout << "General User" << endl;
-}
-
-string User::getId() const {
-    return id;
+    cout << "User" << endl;
 }
 
 string User::getName() const {
     return name;
+}
+
+void User::showInfo() const {
+    cout << "Name: " << name << endl;
+    cout << "Address: " << address << endl;
+    cout << "Email: " << email << endl;
+    cout << "Phone: " << phone << endl;
 }
