e6b05f8ec9dbc0cbd51861d103ad4374640f6a0f
Osama Alfahdi <osamaalfahdi@gmail.com>
2026-05-25 16:05:04 +0400

    Update Student.h

--- Student/Student.h
+++ Student/Student.h
@@ -2,16 +2,26 @@
 #define STUDENT_H
 
 #include "User.h"
+#include <vector>
 
-class Student : public User {       // This is a derived class from User and it's called Student.
+class Student : public User {   // This is a derived class from User and it's called Student.
 private:
-    double grade;
+    string studentID;
+    double Cgpa;
+    vector<string> courses;
+
 public:
     Student();
-    Student(string i, string n);
-    void setGrade(double g);
-    double getGrade() const;
-    void showRole() const override;
+    Student(string id, string n, string a, string e, string p, double c);
+    ~Student();
+
+    void showRole() const;
+
+    void addCourse(string code);
+    void removeCourse(string code);
+    bool hasCourse(string code) const;
+    vector<string> getCourses() const;
+
     void showStudent() const;
 };
 
